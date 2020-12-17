d37d17541200ebfacf10818e754635cb058646d6
David Portilla <david_portilla@hotmail.com>
2020-12-17 09:55:49 -0500

    adding app styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import Header from './components/Header';
+import styled from '@emotion/styled'
+
+const Wrapper = styled.div`
+ max-width:600px;
+ margin: 0 auto;
+`;
+
+const FormWrapper = styled.div`
+  background-color: #fff;
+  padding:3rem;
+`;
 
 function App () {
 
   return (
-    <Header title='Car insurance quotes'></Header >
+    <Wrapper>
+      <Header
+        title='Car insurance quotes'
+      />
+      <FormWrapper>
+        add form here
+      </FormWrapper>
+    </Wrapper>
   );
 }
 
